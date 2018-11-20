a5f534bc3439dabe5c4d4a8c62b872e4ac8eaca5
Josh <grafton.josh@gmail.com>
2018-11-20 15:52:14 -0700

    updated copyright props

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,16 +3,25 @@ import './App.css'
 import Header from '../header/header.js'
 import CartFooter from '../cart-footer/cart-footer.js'
 import Items from '../cart-items/cart-items.js'
+import CartItem from '../cart-item/cart-item.js'
 
 
 class App extends Component {
 
   render() {
+    const cartItemsList =
+    [
+      { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+      { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+      { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+    ]
+    console.log(cartItemsList)
     return (
       <div className='App'>
         <Header />
         <Items />
-        <CartFooter copyright='2018' />
+        <CartItem />
+        <CartFooter copyright='208' />
       </div>
     )
   }
--- src/components/cart-items/cart-items.js
+++ src/components/cart-items/cart-items.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Item from '../cart-item/cart-item'
 
 const Items = () => {
   return (
     <div className="container">
-      <h1>Items</h1>
+      <h1>Cart Items</h1>
+      <div className="list-group">
+        <div className="list-group-item">
+          <div className="row">
+            <div className="col-md-8">Product</div>
+            <div className="col-md-2">Price</div>
+            <div className="col-md-2">Quantity</div>
+          </div>
+        </div>
+        <Item />
+      </div>
     </div>
   )
 }
