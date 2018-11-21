3e529450d6b4be00fb3d57367d540585f725c048
Josh <grafton.josh@gmail.com>
2018-11-21 13:51:06 -0700

    fixed price format

---
+++ src/components/cart-item/cart-item.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class CartItem extends React.Component {
+  render() {
+    const { item } = this.props
+    return (
+      <div className="list-group-item">
+        <div className="row">
+          <div className="col-md-8">{item.product.name}</div>
+          <div className="col-md-2">${item.product.priceInCents/100}</div>
+          <div className="col-md-2">{item.product.id}</div>
+        </div>
+      </div>
+    )
+  }
+}
