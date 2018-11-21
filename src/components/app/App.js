8c91c4acd844d48676e4af8b5ae16b1d910ffda9
Josh <grafton.josh@gmail.com>
2018-11-21 09:51:37 -0700

    added cartItemsList to app.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,19 +10,39 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      products: [
-        { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
-        { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
-        { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
-      ]
-    }
+      cartItemsList: [
+        { id: 1,
+          product: {
+            id: 40,
+            name: 'Mediocre Iron Watch',
+            priceInCents: 399
+          },
+            quantity: 1
+          },
+        { id: 2,
+          product: {
+            id: 41,
+            name: 'Heavy Duty Concrete Plate',
+            priceInCents: 499
+          },
+            quantity: 2
+          },
+        { id: 3, product: {
+          id: 42,
+          name: 'Intelligent Paper Knife',
+          priceInCents: 1999
+        }, quantity: 1
+      },
+    ]
   }
+}
 
   render() {
+    console.log(this.props);
     return (
       <div className='App'>
         <Header />
-        <CartItems cartItems={this.state.products}/>
+        <CartItems items={this.state.products}/>
         <CartFooter copyright='208' />
       </div>
     )
