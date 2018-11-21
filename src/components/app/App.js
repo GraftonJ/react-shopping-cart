6a6a62780f30cc4834ddc73a776d8bc934161a16
Josh <grafton.josh@gmail.com>
2018-11-21 14:41:18 -0700

    working

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,13 +3,24 @@ import './App.css'
 import Header from '../header/header.js'
 import CartFooter from '../cart-footer/cart-footer.js'
 import CartItems from '../cart-items/cart-items.js'
-//import CartItem from '../cart-item/cart-item.js'
+import AddItem from '../add-item/add-item.js'
 
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      products: [
+        { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
+        { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
+        { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
+        { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
+        { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
+        { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
+        { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
+        { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
+        { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
+      ],
       items: [
         { id: 1,
           product: {
@@ -35,9 +46,9 @@ class App extends Component {
           },
             quantity: 1
         },
-    ]
+      ]
+    }
   }
-}
 
   render() {
     console.log(this.props);
@@ -45,6 +56,7 @@ class App extends Component {
       <div className='App'>
         <Header />
         <CartItems items={this.state.items}/>
+        <AddItem products={this.state.products}/>
         <CartFooter copyright='2018' />
       </div>
     )
